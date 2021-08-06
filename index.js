40116cf593337d31a1303aa76218493fe3c3229a
saienko-ana <saienko_ana@icloud.com>
2021-08-06 10:55:26 +0200

    fix: try to modify the code. Still get error messages

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 let reverse = function(num) {
 
-    let n = num;
+    return +num
+    .toString()
+    .split('')
+    .reverse()
+    .join('')
 
-    let digits = n.toString().split("");
+}
 
-    let sorted = digits.reverse();
+/* 
+function dividing(num) {
 
-    let newNum = sorted.join("");
+    let digit;
 
-    let resultNum = parseInt(newNum);
+    while(num) {
+      digit = num % 10;
+      num = Math.floor(num / 10);
 
     }
+  }
+
+  let n = num;
+
+  let digits = n.toString().split("");
+  
+  let sorted = digits.reverse();
+  
+  let newNum = sorted.join("");
+  
+  let resultNum = parseInt(newNum);
+  
+  return resultNum;
+
+  */
+
+  /* 
+    let digit;
+    let resultDiv;
+
+    while(num) {
+      digit = num % 10;
+      resultDiv = num = Math.floor(num / 10);
+    }
+
+    console.log(resultDiv)
+ */
