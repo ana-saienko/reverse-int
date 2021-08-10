6c2bbebb27f7311c35a6dd68bfe2ffdff0a1b12c
ana-saienko <saienko_ana@icloud.com>
2021-08-10 09:39:57 +0200

    fix: finally passed the tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let num = n;
+    let digits = num.toString().split("");
+    let sorted = digits.reverse();
+    let newNum = sorted.join("");
+    let resultNum = parseInt(newNum);
+    return resultNum;
 }
